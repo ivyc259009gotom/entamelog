907a649955eae1ae2f9852498545245531adead0
MiwakoGoto <c259009@ivy.ac.jp>
2026-06-29 15:50:22 +0900

    ユーザー検索機能の作成を開始

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $keyword = $request->input('keyword');
+
+        $users = User::query()
+            ->where('id', '!=', Auth::id())
+            ->when($keyword, function ($query, $keyword) {
+                return $query->where(function ($query) use ($keyword) {
+                    $query->where('name', 'like', '%' . $keyword . '%')
+                        ->orWhere('email', 'like', '%' . $keyword . '%');
+                });
+            })
+            ->latest()
+            ->get();
+
+        return view('users.index', compact('users', 'keyword'));
+    }
+
+    public function show(User $user)
+    {
+        $works = $user->works()
+            ->latest()
+            ->get();
+
+        return view('users.show', compact('user', 'works'));
+    }
+}
